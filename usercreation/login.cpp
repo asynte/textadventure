d9520c70e20ea5d7cc005200590a2f22494596dc
Mike <mariyant@sfu.ca>
2015-10-11 13:16:51 -0700

    prevent duplicate username

--- usercreation/login.cpp
+++ usercreation/login.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include<cstdlib>
 
 using namespace std;
 
@@ -11,7 +12,6 @@ bool Loggedin(){
 	cout<<"Enter password: "; cin>>pass;
 	//search pattern	
 	userpass = user+" "+pass;
-        int offset; 
         string line;
         ifstream usersFile;
         usersFile.open ("userdata.txt"); 
@@ -27,7 +27,7 @@ bool Loggedin(){
 	//close the file
 	usersFile.close(); 
 	if(found) {
-		cout << "Welcome "<< user << '\n';
+		cout << "Welcome to the game "<< user << '\n';
 		//link it to our game server 
 	}
 	else {
@@ -41,12 +41,38 @@ bool Loggedin(){
 int main()
 {
 	int choice;
-	cout<<"1.register\n2:login\nWhat do you want to do? ";cin>>choice;
+	cout<<"\n\nHi there! Welcome to The Game!\n";
+	cout<<"1:Register\n2:Login\nWhat do you want to do? ";cin>>choice;
 
 
 	if(choice == 1){
 		string username,password;
 		cout<<"Select username: "; cin>>username;
+		ifstream fin;
+		fin.open("userdata.txt");
+
+		bool isFound=0;
+		while(!fin.eof()){
+			string temp = "";
+			getline(fin,temp);
+			for(int i=0;i<username.size();i++){
+				if(temp[i]==username[i])
+					isFound = 1;
+					else{
+				isFound =0;
+				break;
+			}
+		}
+		if(isFound)
+		{
+			cout << "Sorry Username already exist!\nTry a different one!\n";
+
+			main();
+		}
+
+	}
+
+
 		cout<<"Select a password: "; cin>>password;
 		ofstream file;
 		file.open("userdata.txt",fstream::app);
@@ -54,6 +80,7 @@ int main()
 		file.close();
 		cout<<"You are registered! Welcome!\n\n";
 		main();
+
 	}
 	else if(choice == 2){
 		bool status = Loggedin();
