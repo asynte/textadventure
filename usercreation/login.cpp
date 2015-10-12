69d73746d98a8a13f28efd6719b412307ecc043a
Mike <mariyant@sfu.ca>
2015-10-12 09:04:30 -0700

    Characterprofile class header

--- usercreation/login.cpp
+++ usercreation/login.cpp
@@ -1,85 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "CharacterProfile.h"
+
 using namespace std;
-
-class CharacterProfile {
-
-public:
-    string username;
-    string password;
-    string user;
-    string pass;
-    
-    // User registration
-    void userRegisterDo()
-    {
-	ofstream file;
-	file.open("userdata.txt",fstream::app);
-	file<<"\n"<<username<< " " <<password;
-	file.close();
-	cout<<"You are registered! Welcome!\n\n";
-    }
-
-    // Initialize user registration
-    void userRegister(){
-        cout << "Enter a new username:\n";
-	cin >> username;
-	ifstream fin;
-	fin.open("userdata.txt");
-	bool isFound=0;
-	while(!fin.eof()){
-		string temp = "";
-		getline(fin,temp);
-		for(int i=0;i<username.size();i++){
-			if(temp[i]==username[i])
-				isFound = 1;
-				else{
-				isFound =0;
-				break;
-				}
-		}
-	}
-	if(isFound){
-		cout << "Sorry Username already exist!\nTry a different one!\n";
-	}else{
-        	cout << "\nPlease enter a new password:\n";
-        	cin >> password;
-		userRegisterDo();	
-		}
-    	}
-
-    bool Loggedin(){
-	string userpass;
-	cout<<"Enter username: "; cin>>user;
-	cout<<"Enter password: "; cin>>pass;
-	//search pattern	
-	userpass = user+" "+pass;
-	string line;
-       	ifstream usersFile;
-        usersFile.open ("userdata.txt"); 
-	bool found = false;
-     	if(usersFile.is_open()) {
-		while(getline(usersFile,line) && !found) {
-			//match any strings
-			if (line.compare(userpass) == 0) { 
-		  	// if data found break loop
-                  	found = true; 
-              		}
-          	}
-	usersFile.close(); 
-	if(found) {
-		//link it to our game server
-		cout << "Welcome to the game "<< user << '\n'; 
-	}else {
-		cout << "\nIncorrect Username and/or password !\n";
-		return false;
-	}
-      	}
-}
-
-};
-
 // Main program
 int main() {
     	int choice;
