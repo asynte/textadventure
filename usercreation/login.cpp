a2478b46d0bc24ab9b1b0cbedf885c9bddf77b27
Mike <mariyant@sfu.ca>
2015-10-12 08:53:00 -0700

    Characterprofile class

--- usercreation/login.cpp
+++ usercreation/login.cpp
@@ -1,93 +1,96 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include<cstdlib>
-
 using namespace std;
 
+class CharacterProfile {
 
-bool Loggedin(){
-	string userpass,user,pass;
+public:
+    string username;
+    string password;
+    string user;
+    string pass;
+    
+    // User registration
+    void userRegisterDo()
+    {
+	ofstream file;
+	file.open("userdata.txt",fstream::app);
+	file<<"\n"<<username<< " " <<password;
+	file.close();
+	cout<<"You are registered! Welcome!\n\n";
+    }
+
+    // Initialize user registration
+    void userRegister(){
+        cout << "Enter a new username:\n";
+	cin >> username;
+	ifstream fin;
+	fin.open("userdata.txt");
+	bool isFound=0;
+	while(!fin.eof()){
+		string temp = "";
+		getline(fin,temp);
+		for(int i=0;i<username.size();i++){
+			if(temp[i]==username[i])
+				isFound = 1;
+				else{
+				isFound =0;
+				break;
+				}
+		}
+	}
+	if(isFound){
+		cout << "Sorry Username already exist!\nTry a different one!\n";
+	}else{
+        	cout << "\nPlease enter a new password:\n";
+        	cin >> password;
+		userRegisterDo();	
+		}
+    	}
+
+    bool Loggedin(){
+	string userpass;
 	cout<<"Enter username: "; cin>>user;
 	cout<<"Enter password: "; cin>>pass;
 	//search pattern	
 	userpass = user+" "+pass;
-        string line;
-        ifstream usersFile;
+	string line;
+       	ifstream usersFile;
         usersFile.open ("userdata.txt"); 
 	bool found = false;
      	if(usersFile.is_open()) {
 		while(getline(usersFile,line) && !found) {
-		//match strings
+			//match any strings
 			if (line.compare(userpass) == 0) { 
 		  	// if data found break loop
                   	found = true; 
               		}
           	}
-	//close the file
 	usersFile.close(); 
 	if(found) {
-		cout << "Welcome to the game "<< user << '\n';
-		//link it to our game server 
-	}
-	else {
+		//link it to our game server
+		cout << "Welcome to the game "<< user << '\n'; 
+	}else {
 		cout << "\nIncorrect Username and/or password !\n";
 		return false;
 	}
-      }
+      	}
 }
 
+};
 
-int main()
-{
-	int choice;
+// Main program
+int main() {
+    	int choice;
+    	CharacterProfile user1;
+    	ifstream usersFile("userdata.txt");
 	cout<<"\n\nHi there! Welcome to The Game!\n";
 	cout<<"1:Register\n2:Login\nWhat do you want to do? ";cin>>choice;
-
-
 	if(choice == 1){
-		string username,password;
-		cout<<"Select username: "; cin>>username;
-		ifstream fin;
-		fin.open("userdata.txt");
-
-		bool isFound=0;
-		while(!fin.eof()){
-			string temp = "";
-			getline(fin,temp);
-			for(int i=0;i<username.size();i++){
-				if(temp[i]==username[i])
-					isFound = 1;
-					else{
-				isFound =0;
-				break;
-			}
-		}
-		if(isFound)
-		{
-			cout << "Sorry Username already exist!\nTry a different one!\n";
-
-			main();
-		}
-
+        	user1.userRegister();
 	}
-
-
-		cout<<"Select a password: "; cin>>password;
-		ofstream file;
-		file.open("userdata.txt",fstream::app);
-		file<<"\n"<<username<< " " <<password;
-		file.close();
-		cout<<"You are registered! Welcome!\n\n";
-		main();
-
-	}
-	else if(choice == 2){
-		bool status = Loggedin();
-		if(!status){
-			cout<<"Please try again!\n"<<endl;
-			main();
-		}
-	}
-	return 0;
+        	else{
+            	user1.Loggedin();
+            	}
 }
